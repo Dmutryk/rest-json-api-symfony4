394800ec72bf8b56f3b3b590f41ddebf4991839d
Vasyl Dmytruk <vasyl.dmytruk@elogic.com.ua>
2019-07-12 00:41:37 +0300

    add migration and cli command

---
+++ src/Migrations/Version20190711164210.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190711164210 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('CREATE TABLE tokens (id VARCHAR(36) NOT NULL, user_id VARCHAR(36) DEFAULT NULL, data CLOB NOT NULL, created_at VARCHAR(30) NOT NULL, expires_at VARCHAR(30) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_AA5A118EA76ED395 ON tokens (user_id)');
+        $this->addSql('CREATE TABLE users (id VARCHAR(36) NOT NULL, username VARCHAR(100) NOT NULL, password VARCHAR(100) NOT NULL, email VARCHAR(100) NOT NULL, roles CLOB NOT NULL --(DC2Type:array)
+        , is_active BOOLEAN NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_1483A5E9F85E0677 ON users (username)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_1483A5E9E7927C74 ON users (email)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('DROP TABLE tokens');
+        $this->addSql('DROP TABLE users');
+    }
+}
